3a5e0e6dafbab226af8f15b4904d865a5b649723
Jude Sajith <65233121+SajithJude@users.noreply.github.com>
2023-03-09 02:53:07 +0000

    sss

--- app.py
+++ app.py
@@ -23,30 +23,30 @@ img_container = {'img':None}
 st.title('Is it Pikachu or Eevee!?')
 st.subheader('The image detection tool you definitely do not need in your life')
 
-# ctx = webrtc_streamer(key="example", 
-#                 video_frame_callback=video_frame_callback,
-#                 rtc_configuration = RTC_CONFIGURATION,
-#                 mode=WebRtcMode.SENDRECV)
-st.write('Dark spots')
-Darkspots = st.progress(0)
-st.write('Puffy Eyes')
-Puffyeyes = st.progress(0)
-st.write('Wrinkles')
-Wrinkles = st.progress(0)
-img_file_buffer = st.camera_input(f"Take a picture ")
-if img_file_buffer is not None:
+col1, col2 = st.columns(2)
 
-    bytes_data = img_file_buffer.getvalue()
-    cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
-    img = cv2.resize(cv2_img, (224, 224), interpolation=cv2.INTER_AREA)
-    img = np.asarray(img, dtype=np.float32).reshape(1, 224, 224, 3)
-    img = (img / 127.5) - 1
-    probabilities = model.predict(img)
-    st.write(probabilities)
-    darkspots = int(probabilities[0,0] * 100)
-    puffyeyes = int(probabilities[0,1] * 100)
-    wrinkles = int(probabilities[0,2] * 100)
+with col1:
+    st.write('Dark spots')
+    Darkspots = st.progress(0)
+    st.write('Puffy Eyes')
+    Puffyeyes = st.progress(0)
+    st.write('Wrinkles')
+    Wrinkles = st.progress(0)
+with col2:
+    img_file_buffer = st.camera_input(f"Take a picture ")
+    if img_file_buffer is not None:
 
-    Darkspots.progress(darkspots)
-    Puffyeyes.progress(puffyeyes)
-    Wrinkles.progress(wrinkles)
+        bytes_data = img_file_buffer.getvalue()
+        cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
+        img = cv2.resize(cv2_img, (224, 224), interpolation=cv2.INTER_AREA)
+        img = np.asarray(img, dtype=np.float32).reshape(1, 224, 224, 3)
+        img = (img / 127.5) - 1
+        probabilities = model.predict(img)
+        st.write(probabilities)
+        darkspots = int(probabilities[0,0] * 100)
+        puffyeyes = int(probabilities[0,1] * 100)
+        wrinkles = int(probabilities[0,2] * 100)
+
+        Darkspots.progress(darkspots)
+        Puffyeyes.progress(puffyeyes)
+        Wrinkles.progress(wrinkles)
