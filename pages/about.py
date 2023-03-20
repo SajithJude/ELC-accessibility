eba57c23ff6af4498f9ce823348a3a399f3cc002
Jude Sajith <judesajith.aj@gmail.com>
2023-03-20 13:55:22 -0400

    smsmsm

--- pages/about.py
+++ pages/about.py
@@ -2,29 +2,29 @@ import streamlit as st
 
 st.subheader("Inspiration")
 
-st.markdown("Many people, including those with neurogenerative disorders like OCD, ADHD, and PTSD, struggle with decision-making when it comes to beauty and makeup products. The overwhelming number of options, combined with the pressure to choose the "right" product, can be paralyzing. We wanted to create an app that would use AI and image processing to suggest the exact product needed, reducing decision fatigue and making the process of selecting and purchasing products easier and less overwhelming.")
+st.write("Many people, including those with neurogenerative disorders like OCD, ADHD, and PTSD, struggle with decision-making when it comes to beauty and makeup products. The overwhelming number of options, combined with the pressure to choose the "right" product, can be paralyzing. We wanted to create an app that would use AI and image processing to suggest the exact product needed, reducing decision fatigue and making the process of selecting and purchasing products easier and less overwhelming.")
 
 
 st.subheader("What it does")
 
-st.markdown("Beautify.ai uses AI and image processing to suggest the exact beauty and makeup products needed, based on a user's individual needs and preferences. The app provides step-by-step instructions on using the product, specifically insighting which area of the skin to apply, the quantity, and grooming tips. This feature is particularly helpful for individuals with neurogenerative disorders, who may need more guidance or structure when it comes to grooming and beauty routines.")
+st.write("Beautify.ai uses AI and image processing to suggest the exact beauty and makeup products needed, based on a user's individual needs and preferences. The app provides step-by-step instructions on using the product, specifically insighting which area of the skin to apply, the quantity, and grooming tips. This feature is particularly helpful for individuals with neurogenerative disorders, who may need more guidance or structure when it comes to grooming and beauty routines.")
 
 
 st.subheader("How we built it")
 
-st.markdown("We built Beautify.ai using a combination of machine learning algorithms, image processing techniques, and user-centered design principles. We worked closely with individuals with neurogenerative disorders to ensure that the app would be accessible and easy to use for everyone.")
+st.write("We built Beautify.ai using a combination of machine learning algorithms, image processing techniques, and user-centered design principles. We worked closely with individuals with neurogenerative disorders to ensure that the app would be accessible and easy to use for everyone.")
 
 st.subheader("Challenges we ran into")
 
-st.markdown("One of the biggest challenges we faced was ensuring that the app was accessible and easy to use for individuals with neurogenerative disorders. We conducted extensive user testing and made many design iterations based on feedback from our testers. We also had to carefully consider ethical and privacy concerns when designing the app.")
+st.write("One of the biggest challenges we faced was ensuring that the app was accessible and easy to use for individuals with neurogenerative disorders. We conducted extensive user testing and made many design iterations based on feedback from our testers. We also had to carefully consider ethical and privacy concerns when designing the app.")
 
 
 st.subheader("Accomplishments that we're proud of")
 
-st.markdown("We're proud to have created an app that has the potential to make a real difference in the lives of individuals with neurogenerative disorders. We're also proud of the user-centered design process we used to create the app,  which ensured that it would be accessible and easy to use for everyone.")
+st.write("We're proud to have created an app that has the potential to make a real difference in the lives of individuals with neurogenerative disorders. We're also proud of the user-centered design process we used to create the app,  which ensured that it would be accessible and easy to use for everyone.")
 
 
 
 st.subheader("What we learned")
 
-st.markdown("We learned a lot about the challenges that individuals with neurogenerative disorders face when it comes to decision-making and grooming routines")
+st.write("We learned a lot about the challenges that individuals with neurogenerative disorders face when it comes to decision-making and grooming routines")
